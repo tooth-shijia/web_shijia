c1c182deb422a034417677fcf7feba20be3e292e
Tangxinqi <tangxinqi313313@163.com>
2016-06-05 21:35:02 +0800

    主页面信息

--- src/main/java/com/shijia/web/controller/user/BaseController.java
+++ src/main/java/com/shijia/web/controller/user/BaseController.java
@@ -1,9 +1,13 @@
 package com.shijia.web.controller.user;
 
+import com.shijia.web.common.framework.HttpContext;
 import com.shijia.web.common.utils.DateUtils;
+import org.apache.commons.httpclient.HttpClient;
+import org.springframework.http.HttpRequest;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Calendar;
 
 /**
@@ -16,7 +20,13 @@ public class BaseController {
 
 
         modelAndView.addAttribute("time","<div>"+DateUtils.getTodayForString()+"</div>");
-
+        if(true){
+            String login = "<div>"
+                    +"<a href=\"/shijia/login.html\"><img src=\"/uimg/loginico.png\" >Login</a>"
+                    +"<a href=\"/shijia/signup.html\"><img src=\"/uimg/signup.png\" >Sign up</a>"
+                    +"</div>";
+            modelAndView.addAttribute("login",login);
+        }
 
     }
 }
--- src/main/java/com/shijia/web/controller/user/MainPageController.java
+++ src/main/java/com/shijia/web/controller/user/MainPageController.java
@@ -2,6 +2,7 @@ package com.shijia.web.controller.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Tangxinqi on 2016/6/3.
@@ -11,9 +12,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainPageController  extends  BaseController{
 
     @RequestMapping("/index")
-    public  String moveToMainPage(){
-
-         return "user/index";
+    public  ModelAndView moveToMainPage(){
+         ModelAndView model = new ModelAndView("user/index");
+         model.addObject("m_title","上海世佳义齿");
+         return model;
      }
 
+    @RequestMapping("/login")
+    public  ModelAndView moveToLoginPage(){
+        ModelAndView model = new ModelAndView("user/login");
+        model.addObject("m_title","上海世佳义齿");
+        return model;
+    }
+
+    @RequestMapping("/signup")
+    public  ModelAndView moveToSignupPage(){
+        ModelAndView model = new ModelAndView("user/signup");
+        model.addObject("m_title","上海世佳义齿");
+        return model;
+    }
 }

cbf404ad212a1e9349ea31cce1f0296b18269e7f
Tangxinqi <tangxinqi313313@163.com>
2016-06-11 16:41:40 +0800

    生成验证码后天

---
+++ src/main/java/com/shijia/web/service/servlet/GetYzmServlet.java
@@ -0,0 +1,37 @@
+package com.shijia.web.service.servlet;
+
+import com.shijia.web.common.utils.DrawYzmUtils;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * Created by Tangxinqi on 2016/6/10.
+ */
+public class GetYzmServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+
+        BufferedImage img = new BufferedImage(120,40,BufferedImage.TYPE_INT_ARGB_PRE);
+        DrawYzmUtils draw = new DrawYzmUtils(120,40);
+        String code  = draw.FormatYzmImage(img);
+        req.getSession().setAttribute("checkcode", code);
+        ImageIO.write(img, "png", resp.getOutputStream());
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+
+
+
+    }
+}

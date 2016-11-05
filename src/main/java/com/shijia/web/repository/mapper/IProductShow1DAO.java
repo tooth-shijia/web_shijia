b4a12fb0e98ba0ee58e95765620bf799bd8dac97
lianfang.tan <lianfang.tan@corp.elong.com>
2016-11-06 02:05:58 +0800

    整理配置相关

---
+++ src/main/java/com/shijia/web/repository/mapper/IProductShow1DAO.java
@@ -0,0 +1,11 @@
+package com.shijia.web.repository.mapper;
+
+import com.shijia.web.repository.mapper.domain.ProductType;
+
+/**
+ * @author YanxiSir
+ * @since 16/11/6
+ */
+public interface IProductShow1DAO {
+    public int addProductType(ProductType productType);
+}

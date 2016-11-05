55e13149b1b9df73e596f6fd7e39d852a60d9327
lianfang.tan <lianfang.tan@corp.elong.com>
2016-11-06 01:14:58 +0800

    更新mapper管理

---
+++ src/main/java/com/shijia/web/repository/mapper/INewsMapper.java
@@ -0,0 +1,15 @@
+package com.shijia.web.repository.mapper;
+
+import com.shijia.web.repository.mapper.domain.NewsShow;
+
+/**
+ * @author YanxiSir
+ * @since 16/11/6
+ */
+public interface INewsMapper {
+
+    /**
+     * NewsShow
+     */
+    public int addNewsShow(NewsShow newsShow);
+}
---
+++ src/main/java/com/shijia/web/repository/mapper/IProductTypeMapper.java
@@ -0,0 +1,15 @@
+package com.shijia.web.repository.mapper;
+
+import com.shijia.web.repository.mapper.domain.ProductType;
+
+/**
+ * @author YanxiSir
+ * @since 16/11/6
+ */
+public interface IProductTypeMapper {
+
+    /**
+     * ProductType
+     */
+    public int addProductType(ProductType productType);
+}

5ab29de4d4837848428df9d5b93545ab716ade0d
v_tangxinqi <v_tangxinqi@baidu.com>
2017-01-05 02:01:48 +0800

    样式整理

--- src/main/java/com/shijia/web/controller/user/MenuListPageController.java
+++ src/main/java/com/shijia/web/controller/user/MenuListPageController.java
@@ -9,12 +9,25 @@ import org.springframework.web.servlet.ModelAndView;
  * Created by Tangxinqi on 2016/7/3.
  */
 @Controller
-@RequestMapping("web/shijia/companyinfo")
 public class MenuListPageController {
 
-    @RequestMapping("/gsjj")
+    @RequestMapping("web/shijia/companyinfo/gsjj")
     public ModelAndView moveToCompanyInfoPage(){
         ModelAndView model = new ModelAndView("user/companyinfo/gsjj");
+        model.addObject("m_title","上海世佳义齿");
+        return model;
+    }
+
+    @RequestMapping("web/shijia/companyinfo/qywh")
+    public ModelAndView moveToCompanyCulture(){
+        ModelAndView model = new ModelAndView("user/companyinfo/qywh");
+        model.addObject("m_title","上海世佳义齿");
+        return model;
+    }
+
+    @RequestMapping("web/shijia/product/shijiaproduct")
+    public ModelAndView moveToproductShow(){
+        ModelAndView model = new ModelAndView("user/product/shijiaproduct");
         model.addObject("m_title","上海世佳义齿");
         return model;
     }
